b532fd9aebd7470e3daa57c4a2369dcd62a799a7
Nishapal8285 <92815023+Nishapal8285@users.noreply.github.com>
2021-11-06 21:33:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+// import Prac1 from './Prac1';
+// import Counter from './Counter';
+// import Fetch from './Fetch';
+// import Pracfetch from './Pracfetch';
+import Assign1 from './Assign1';
+// import Props from './Props';
+// import Mounting from './Mounting';
+
+
+function App() {
+//  const handle=(abc)=>{
+//     alert(`hello ${abc}`)
+//   }
+
+
+  return (
+    <div>
+      
+      <Assign1 />
+      {/* <Mounting /> */}
+    </div>
+  
+  )
+  }
+export default App;
