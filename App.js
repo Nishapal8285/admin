4efeb9b1c32fcdfc3865925b53243566dfce8a26
Nishapal8285 <92815023+Nishapal8285@users.noreply.github.com>
2021-11-06 21:14:57 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Assign1 from './Assign1';
+
+
+
+function App() {
+//  const handle=(abc)=>{
+//     alert(`hello ${abc}`)
+//   }
+
+
+  return (
+    <div>
+      
+      <Assign1 />
+      {/* <Mounting /> */}
+    </div>
+  
+  )
+  }
+export default App;
